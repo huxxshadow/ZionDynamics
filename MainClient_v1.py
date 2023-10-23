9e3a0ff244dee7e2a2ce117b4a337e12917771e5
tyrannnnnt <1095144689@qq.com>
2023-10-23 23:04:17 +0800

    clint

--- MainClient_v1.py
+++ MainClient_v1.py
@@ -1,17 +1,25 @@
 import asyncio
+import io
 import os
 import time
 import wave
 
+import numpy as np
 import sherpa_ncnn
 import sys
 import sounddevice as sd
+import soundfile
 import soundfile as sf
 from gtts import gTTS
 import pygame
 import socket
 from threading import Thread
 import threading
+import struct
+import pickle
+import logging
+import base64
+from config_ import record_filler_len, data_package_size
 
 code_path = os.path.dirname(os.path.abspath(__file__))
 mp3_path = os.path.join(code_path, "mp3.mp3")
@@ -19,32 +27,8 @@ STRING_SPECIFIER = "2222"
 WAV_SPECIFIER = "3333"
 exit = False
 
-
-
-
-# def TTS(response, start_time):
-#     tts = gTTS(text=response, lang='en')  # 英文 "en", 普通话 "zh-CN", 粤语 "zh-yue", 日语 "ja"
-#     if os.path.exists(mp3_path):
-#         os.remove(mp3_path)
-#     tts.save(mp3_path)
-
-# running_time2 = time.time() - start_time
-# print("TTS running time:", running_time2, "seconds")
-
-
-# def play_mp3(file_path):
-#     pygame.init()
-#     pygame.mixer.init()
-#     pygame.mixer.music.load(file_path)
-#     pygame.mixer.music.play()
-#     # running_time2 = time.time() - start_time
-#     # print("play_mp3 running time:", running_time2, "seconds")
-#     while pygame.mixer.music.get_busy():
-#         continue
-#
-#     pygame.mixer.music.stop()
-#     pygame.mixer.quit()
-#     pygame.quit()
+logging.basicConfig(format='[%(name)s] %(levelname)s: %(message)s', level=logging.INFO)
+logger = logging.getLogger("客户端")
 
 
 def create_recognizer():
@@ -109,16 +93,57 @@ def sound_echo(recognizer, sample_rate, samples_per_read):
 
 def receiveMsg():
     totalData = bytes()
+    sock.settimeout(5)
     while True:
-        data = sock.recv(1024)
-        totalData += data
-        if len(data) < 1024:
-            break
+        try:
+            data = sock.recv(1024)
+            print(len(data))
+            if not data:
+                break
+            totalData += data
+            # if len(data) < 1024:
+            #     break
+        except socket.timeout:
+            # Handle timeout by retransmitting the lost package
+            sock.send(b'RETRANSMIT')
+    print(len(totalData))
     return totalData
 
 
+def write_package_to_file(writer, data_package):
+    # 将1024个字节 base64编码数据解码为 768个字节utf8数据
+    recv_data = base64.b64decode(data_package)
+    logger.debug(f"接收的数据为: {recv_data}")
+    logger.debug(f"接收的数据长度为: {len(recv_data)}")
+
+    # 字节流读取数据
+    recv_data_stream = io.BytesIO(recv_data)
+    # 返回一个对应于缓冲区内容的可读写视图而不必拷贝其数据
+    recv_data_stream = recv_data_stream.getvalue()
+
+    # 获取填充字符的长度和记录填充字符所占用的长度 根据字节截取想要的数据
+    fill_char_len = recv_data_stream[-record_filler_len:].decode()
+    cut_len = int(fill_char_len) + record_filler_len
+    recv_real_data = recv_data[:-cut_len]
+    # 返回数据包的字节数据
+    writer.write(recv_real_data)
+
+
+def receiveWAV(path):
+    with open(path, "wb") as wf:
+        while True:
+            data_package = sock.recv(data_package_size)
+            write_package_to_file(wf, data_package)
+            # 文件传输结束
+            if not data_package:
+                logger.info(f"{path} 下载完成！！")
+                break
+
+
 def getData():
+    print("********************************************************")
     specifier = str(receiveMsg(), encoding="utf-8")
+    print("receive the specifier: " + specifier)
     if specifier == STRING_SPECIFIER:
         receivedStr = str(receiveMsg(), encoding="utf-8")
         print(receivedStr)
@@ -126,7 +151,6 @@ def getData():
 
     elif specifier == WAV_SPECIFIER:
         # array with parameters
-
         # ww = wave.open('received.wav', 'wb')
         # ww.setnchannels(1)
         # ww.setsampwidth(2)
@@ -139,9 +163,25 @@ def getData():
         # ww.writeframes(songs)
         # ww.close()
         # print("received wav file")
-        with open('received.wav', 'wb') as file:
-            data = receiveMsg()
-            file.write(data)
+
+        # with open('received.wav', 'wb') as file:
+        #     data = receiveMsg()
+        #     file.write(data)
+        #     print("This is the write wav stage.")
+        #     print(data)
+        # sf.write('received.wav', np.frombuffer(literal_eval(str(receiveMsg(), encoding='utf-8')), samplerate=44100))
+        # rate = pickle.loads(receiveMsg())
+        # print(rate)
+        # re1 = receiveMsg()
+        # print(re1)
+        # print(str(len(re1)))
+        # data = pickle.loads(re1)
+        # soundfile.write('received.wav', data,  samplerate=rate)
+        receiveWAV('received.wav')
+
+
+
+
 
 
 def play_wav(file_path):
@@ -155,25 +195,24 @@ def play_wav(file_path):
 def processMsg():
     global last_result, length_last
     event.clear()
-    if len(last_result)-length_last<5:
+    if len(last_result) - length_last < 5:
         event.set()
         return "voiceInput:"
-
 
     temp = last_result[length_last:len(last_result)]
     length_last = len(last_result)
     event.set()
+    print("This is the processMsg() stage")
     return "voiceInput:" + temp
 
 
 def sendString(msg):
     print("this is the send message: " + msg)
-    sock.sendall(bytes(STRING_SPECIFIER,encoding="utf-8"))
+    sock.sendall(bytes(STRING_SPECIFIER, encoding="utf-8"))
     time.sleep(0.1)
     if len(msg) % 1024 == 0:
         msg = msg + " "
     sock.sendall(bytes(msg, encoding="utf-8"))
-
 
 
 class keepMonitor(Thread):
@@ -191,16 +230,19 @@ class keepMonitor(Thread):
         with sd.InputStream(channels=1, dtype="float32", samplerate=sample_rate) as s:
             event.set()
             while not exit:
-                event.wait()
-                if(len(last_result)>500):
+
+                if (len(last_result) > 500):
                     list(last_result).pop(0)
+                event.wait()
                 samples, _ = s.read(samples_per_read)  # a blocking read
                 samples = samples.reshape(-1)
+                event.wait()
                 recognizer.accept_waveform(sample_rate, samples)
                 result = recognizer.text
                 if last_result != result:
                     last_result = result
                     # print("\r{}".format(result), end="", flush=True)
+
 
 class keepReceiveMsg(Thread):
     def __init__(self, name):
@@ -214,9 +256,12 @@ class keepReceiveMsg(Thread):
             input_Msg = processMsg()
             if input_Msg == "voiceInput:":
                 continue
+            print("receive kazhu difang 1")
             sendString(input_Msg)
             time.sleep(0.05)
+            print("receive kazhu difang 2")
             msg = getData()
+            print("receive kazhu difang 3")
             play_wav("received.wav")
             time.sleep(4)
             # processedMsg = handleMsg(msg)
@@ -226,7 +271,7 @@ class keepReceiveMsg(Thread):
             # print("the sleep 5s hou")
 
 
-global last_result,length_last
+global last_result, length_last
 length_last = 0
 last_result = ""
 
@@ -234,31 +279,31 @@ last_result = ""
 # sd.default.device[1] = 4
 
 sock = socket.socket()
-sock.connect(('172.28.162.150', 9007))
-event=threading.Event()
+sock.connect(('172.28.162.150', 9008))
+# sock.connect(('192.168.137.138', 9007))
+
+event = threading.Event()
 event.clear()
 tMonitor = keepMonitor("Monitor")
 tRec = keepReceiveMsg("Receive_Msg")
-
 
 # tSend = threading.Thread(target=mainSendMsg(), name="MainSendMsg")
 tRec.start()
 tMonitor.start()
 
-    # tSend.start()
+# tSend.start()
 
 
+# tSend.join()
 
-    # tSend.join()
-
-    # recognizer, sample_rate, samples_per_read = soundInput_initial()
-    #
-    # while True:
-    #     try:
-    #         recognizer = create_recognizer()
-    #         result = sound_echo(recognizer, sample_rate, samples_per_read)
-    #     except KeyboardInterrupt:
-    #         print("\nCaught Ctrl + C. Exiting")
-    #
-    #     send_msg = "voiceInput:" + result
-    #     asyncio.run(tcp_echo_client(send_msg))
+# recognizer, sample_rate, samples_per_read = soundInput_initial()
+#
+# while True:
+#     try:
+#         recognizer = create_recognizer()
+#         result = sound_echo(recognizer, sample_rate, samples_per_read)
+#     except KeyboardInterrupt:
+#         print("\nCaught Ctrl + C. Exiting")
+#
+#     send_msg = "voiceInput:" + result
+#     asyncio.run(tcp_echo_client(send_msg))
