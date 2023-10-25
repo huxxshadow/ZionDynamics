c7afe68c162bc66102668ef0dd38d703be861b55
huxx <huxxshadowhzy@gmail.com>
2023-10-25 22:15:23 +0800

    initial

--- playVideo.py
+++ playVideo.py
@@ -1,13 +1,18 @@
-class Video(object):
-    def __init__(self,path):
-        self.path = path
+# class Video(object):
+#     def __init__(self,path):
+#         self.path = path
+#
+#     def play(self):
+#         from os import startfile
+#         startfile(self.path)
+#
+# class Movie_MP4(Video):
+#     type = 'MP4'
+#
+# movie = Movie_MP4(r'0af4fdca8d0a62772eba074344790e70.mp4')
+# movie.play()
 
-    def play(self):
-        from os import startfile
-        startfile(self.path)
+import vlc
 
-class Movie_MP4(Video):
-    type = 'MP4'
-
-movie = Movie_MP4(r'0af4fdca8d0a62772eba074344790e70.mp4')
-movie.play()
+p = vlc.MediaPlayer("test.mp3")
+p.play()
