77d7915a7ffc84e08a913780d3a10108703a9bbe
DING Lianli <21251746@life.hkbu.edu.hk>
2023-10-27 20:52:17 +0800

    play video update

--- playVideo.py
+++ playVideo.py
@@ -5,6 +5,25 @@ from threading import Thread
 class keepPlayingV(Thread):
     signal = 0
 
+    # creating Instance class object
+    player = vlc.Instance()
+
+    # creating a new media list
+    media_list = player.media_list_new()
+
+    # creating a media player object
+    media_player = player.media_list_player_new()
+
+    # creating a new media - todo: media list
+    MainMedia = player.media_new("C:\\Users\\Hp User\\Desktop\\test1.mp4")
+    smile = player.media_new("...")
+
+    # adding media to media list
+    media_list.add_media(MainMedia)
+
+    # setting media list to the mediaplayer
+    media_player.set_media_list(media_list)
+
     def __init__(self, name, signal):
         super().__init__()
         self.name = name
@@ -18,14 +37,19 @@ class keepPlayingV(Thread):
 
     def playRepeatedly(self):
         # main video
-        p = vlc.MediaPlayer("C:\\Users\\Hp User\\Desktop\\test1.mp4")
-        while True:
-            p.play()
+        # p = vlc.MediaPlayer("C:\\Users\\Hp User\\Desktop\\test1.mp4")
+        # setting loop
+        self.media_player.vlm_set_loop("mainLoop", True)
+        self.player.play()
 
     def playOnce(self, signal):
         path = self.switch(signal)
-        p = vlc.MediaPlayer(path)
-        p.play()
+        # p = vlc.MediaPlayer(path)
+        # self.player.play()
+        # p.play()
+        # playing the media
+        
+        self.player.vlm_play_media("death_note")
 
     def switch(self, signal):
         if signal == 1:
